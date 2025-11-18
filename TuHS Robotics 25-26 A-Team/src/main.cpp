2f9d8067dc7dd7e1f4961103f4b16903028db3af
QuasarK <96966687+quasark-main@users.noreply.github.com>
2025-11-17 16:34:26 -0800

    Added drivetrain functionality
    Added motors and tank drive

--- TuHS Robotics 25-26 A-Team/src/main.cpp
+++ TuHS Robotics 25-26 A-Team/src/main.cpp
@@ -7,6 +7,17 @@ const string REPLAY_FILE = "/usd/a_team_auton_25_26.txt";
 pros::Controller playerController(pros::E_CONTROLLER_MASTER);
 ReplayController replayController(REPLAY_FILE);
 
+int DRIVE_MAX_SPEED = 100;
+
+//MOTOR DEFINITIONS
+pros::Motor right_front(1, true);
+pros::Motor right_middle(2, true);
+pros::Motor right_back(3, true);
+pros::Motor left_front(11);
+pros::Motor left_middle(19);
+pros::Motor left_back(20);
+
+
 
 void initialize() {
 	pros::lcd::initialize();
@@ -14,18 +25,15 @@ void initialize() {
 }
 
 
-
 void disabled() {}
-
 
 
 void competition_initialize() {}
 
 
-
 void autonomous() {
 	theFile.open(REPLAY_FILE, std::ios_base::in);
-	while(true) {
+	while(true) {right_front
 		replayController.updateFrame(false);
 		drive(replayController);
 		pros::delay(20);
@@ -35,15 +43,26 @@ void autonomous() {
 }
 
 
-
 void opcontrol() {
 	while (true) {
+		drive(playerController);
 		pros::delay(20);
 	}
 }
 
 
-
 void drive(auto master){
+	int left_joystick = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+	int right_joystick = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
 
+	int right_speed = right_joystick * (DRIVE_MAX_SPEED / 127.0);
+	int left_speed = left_joystick * (DRIVE_MAX_SPEED / 127.0);
+
+	right_front.move(right_speed);
+	right_middle.move(right_speed);
+	right_back.move(right_speed);
+
+	left_front.move(left_speed);
+	left_middle.move(left_speed);
+	left_back.move(left_speed);
 }
